17af1687693eb8b1cb5764ac60470711924fca83
Christian Vanderwall <christian@cvdub.net>
2020-08-02 21:56:54 -0700

    Start chapter 25

---
+++ my-code/id3/packages.lisp
@@ -0,0 +1,19 @@
+(defpackage :net.cvdub.id3v2
+  (:use :common-lisp
+   :net.cvdub.pathnames
+        :net.cvdub.binary)
+  (:export
+   :read-id3
+   :mp3-p
+   :id3-p
+   :album
+   :composer
+   :genre
+   :encoding-program
+   :artist
+   :part-of-set
+   :track
+   :song
+   :year
+   :size
+   :translated-genre))
