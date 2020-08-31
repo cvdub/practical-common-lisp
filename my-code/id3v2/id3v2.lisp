ac16779f544a593a0a39a0f6f2f39af9c275e9f0
Christian Vanderwall <christian@cvdub.net>
2020-08-30 19:32:39 -0700

    Continue work on chapter 25

--- my-code/id3v2/id3v2.lisp
+++ my-code/id3v2/id3v2.lisp
@@ -111,13 +111,6 @@
             :terminator terminator
             :character-type (ucs2-char-type #xfeff))))
 
-(define-binary-class id3-tag ()
-  ((identifier (iso-8859-1-string :length 3))
-   (major-version u1)
-   (revision u1)
-   (flags u1)
-   (size id3-tag-size)))
-
 (defun read-id3 (file)
   (with-open-file (in file :element-type '(unsigned-byte 8))
     (read-value 'id3-tag in)))
@@ -126,3 +119,108 @@
   (with-slots (identifier major-version revision flags size) (read-id3 file)
     (format t "~a ~d.~d ~8,'0b ~d bytes -- ~a~%"
             identifier major-version revision flags size (enough-namestring file))))
+
+(defun mp3-p (file)
+  (and
+   (not (directory-pathname-p file))
+   (string-equal "mp3" (pathname-type file))))
+
+(defun show-tag-headers (dir)
+  (walk-directory dir #'show-tag-header :test #'mp3-p))
+
+(defun count-versions (dir)
+  (let ((versions (mapcar #'(lambda (x) (cons x 0)) '(2 3 4))))
+    (flet ((count-version (file)
+             (incf (cdr (assoc (major-version (read-id3 file)) versions)))))
+      (walk-directory dir #'count-version :test #'mp3-p))
+    versions))
+
+(defun id3-p (file)
+  (with-open-file (in file :element-type '(unsigned-byte 8))
+    (string= "ID3" (read-value 'iso-8859-1-string in :length 3))))
+
+(define-binary-class generic-frame (id3-frame)
+  ((data (raw-bytes :size size))))
+
+(define-binary-type raw-bytes (size)
+  (:reader (in)
+           (let ((buf (make-array size :element-type '(unsigned-byte 8))))
+             (read-sequence buf in)
+             buf))
+  (:writer (out buf)
+           (write-sequence buf out)))
+
+(defun find-frame-class (id)
+  (declare (ignore id))
+  'generic-frame)
+
+(define-condition in-padding () ())
+
+(define-binary-type frame-id (length)
+  (:reader (in)
+           (let ((first-byte (read-byte in)))
+             (when (= first-byte 0) (signal 'in-padding))
+             (let ((rest (read-value 'iso-8859-1-string in :length (1- length))))
+               (concatenate
+                'string (string (code-char first-byte)) rest))))
+  (:writer (out id)
+           (write-value 'iso-8859-1-string out id :length length)))
+
+(define-tagged-binary-class id3-frame ()
+  ((id (frame-id :length 3))
+   (size u3))
+  (:dispatch (find-frame-class id)))
+
+(define-tagged-binary-class id3-tag ()
+  ((identifier (iso-8859-1-string :length 3))
+   (major-version u1)
+   (revision u1)
+   (flags u1)
+   (size id3-tag-size))
+  (:dispatch
+   (ecase major-version
+     (2 'id3v2.2-tag)
+     (3 'id3v2.3-tag))))
+
+(define-binary-class id3v2.2-tag (id3-tag)
+  ((frames (id3-frames :tag-size size :frame-type 'id3v2.2-frame))))
+
+(define-binary-type optional (type if)
+  (:reader (in)
+           (when if (read-value type in)))
+  (:writer (out value)
+           (when if (write-value type out value))))
+
+(define-binary-class id3v2.3-tag (id3-tag)
+                     ((extended-header-size (optional :type 'u4 :if (extended-p flags)))
+                      (extra-flags (optional :type 'u2 :if (extended-p flags)))
+                      (padding-size (optional :type 'u4 :if (extended-p flags)))
+                      (crc (optional :type 'u4 :if (crc-p flags extra-flags)))
+                      (frames (id3-frames :tag-size size :frame-type 'id3v2.3-frame))))
+
+(defun extended-p (flags) (logbitp 6 flags))
+
+(defun crc-p (flags extra-flags)
+  (and (extended-p flags) (logbitp 15 extra-flags)))
+
+(define-binary-type id3-frames (tag-size frame-type)
+  (:reader (in)
+           (loop with to-read = tag-size
+                 while (plusp to-read)
+                 for frame = (read-frame frame-type in)
+                 while frame
+                 do (decf to-read (+ (frame-header-size frame) (size frame)))
+                 collect frame
+                 finally (loop repeat (1- to-read) do (read-byte in))))
+  (:writer (out frames)
+           (loop with to-write = tag-size
+                 for frame in frames
+                 do (write-value frame-type out frame)
+                    (decf to-write (+ (frame-header-size frame) (size frame)))
+                 finally (loop repeat to-write do (write-byte 0 out)))))
+
+(defun read-frame (frame-type in)
+  (handler-case (read-value frame-type in)
+    (in-padding () nil)))
+
+(defgeneric frame-header-size (frame))
