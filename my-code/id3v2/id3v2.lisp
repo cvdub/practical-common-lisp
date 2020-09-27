a99f7b93a2d4b32d40808bf03464bd9d41e73c38
Christian Vanderwall <christian@cvdub.net>
2020-09-27 16:52:49 -0700

    Chapter 25 up to "Extracting Information from an ID3 Tag"

--- my-code/id3v2/id3v2.lisp
+++ my-code/id3v2/id3v2.lisp
@@ -12,13 +12,13 @@
 
 (define-binary-type unsigned-integer (bytes bits-per-byte)
   (:reader (in)
-           (loop with value = 0
-                 for low-bit downfrom (* bits-per-byte (1- bytes)) to 0 by bits-per-byte do
-                   (setf (ldb (byte bits-per-byte low-bit) value) (read-byte in))
-                 finally (return value)))
+    (loop with value = 0
+          for low-bit downfrom (* bits-per-byte (1- bytes)) to 0 by bits-per-byte do
+            (setf (ldb (byte bits-per-byte low-bit) value) (read-byte in))
+          finally (return value)))
   (:writer (out value)
-           (loop for low-bit downfrom (* bits-per-byte (1- bytes)) to 0 by bits-per-byte
-                 do (write-byte (ldb (byte bits-per-byte low-bit) value) out))))
+    (loop for low-bit downfrom (* bits-per-byte (1- bytes)) to 0 by bits-per-byte
+          do (write-byte (ldb (byte bits-per-byte low-bit) value) out))))
 
 (define-binary-type u1 () (unsigned-integer :bytes 1 :bits-per-byte 8))
 (define-binary-type u2 () (unsigned-integer :bytes 2 :bits-per-byte 8))
@@ -28,36 +28,36 @@
 
 (define-binary-type generic-string (length character-type)
   (:reader (in)
-           (let ((string (make-string length)))
-             (dotimes (i length)
-               (setf (char string i) (read-value character-type  in)))
-             string))
+    (let ((string (make-string length)))
+      (dotimes (i length)
+        (setf (char string i) (read-value character-type  in)))
+      string))
   (:writer (out string)
-           (dotimes (i length)
-             (write-value character-type out (char string i)))))
+    (dotimes (i length)
+      (write-value character-type out (char string i)))))
 
 (define-binary-type generic-terminated-string (terminator character-type)
   (:reader (in)
-           (with-output-to-string (s)
-             (loop for char = (read-value character-type in)
-                   until (char= char terminator) do (write-char char s))))
+    (with-output-to-string (s)
+      (loop for char = (read-value character-type in)
+            until (char= char terminator) do (write-char char s))))
   (:writer (out string)
-           (loop for char across string
-                 do (write-value character-type out char)
-                 finally (write-value character-type out terminator))))
+    (loop for char across string
+          do (write-value character-type out char)
+          finally (write-value character-type out terminator))))
 
 (define-binary-type iso-8859-1-char ()
   (:reader (in)
-           (let ((code (read-byte in)))
-             (or (code-char code)
-                 (error "Character code ~d not supported" code))))
+    (let ((code (read-byte in)))
+      (or (code-char code)
+          (error "Character code ~d not supported" code))))
   (:writer (out char)
-           (let ((code (char-code char)))
-             (if (<= 0 code #xff)
-                 (write-byte code out)
-                 (error
-                  "Illegal character for iso-88559-1 encoding: character: ~c with code: ~d"
-                  char code)))))
+    (let ((code (char-code char)))
+      (if (<= 0 code #xff)
+          (write-byte code out)
+          (error
+           "Illegal character for iso-88559-1 encoding: character: ~c with code: ~d"
+           char code)))))
 
 (define-binary-type iso-8859-1-string (length)
   (generic-string :length length :character-type 'iso-8859-1-char))
@@ -68,16 +68,16 @@
 
 (define-binary-type ucs-2-char (swap)
   (:reader (in)
-           (let ((code (read-value 'u2 in)))
-             (when swap (setf code (swap-bytes code)))
-             (or (code-char code) (error "Character code ~d not supported" code))))
+    (let ((code (read-value 'u2 in)))
+      (when swap (setf code (swap-bytes code)))
+      (or (code-char code) (error "Character code ~d not supported" code))))
   (:writer (out char)
-           (let ((code (char-code char)))
-             (unless (<= 0 code #xffff)
-               (error "Illegal character for ucs-2 encoding: ~c with char-code: ~d"
-                      char code))
-             (when swap (setf code (swap-bytes code)))
-             (write-value 'u2 out code))))
+    (let ((code (char-code char)))
+      (unless (<= 0 code #xffff)
+        (error "Illegal character for ucs-2 encoding: ~c with char-code: ~d"
+               char code))
+      (when swap (setf code (swap-bytes code)))
+      (write-value 'u2 out code))))
 
 (defun swap-bytes (code)
   (assert (<= code #xffff))
@@ -94,32 +94,32 @@
 
 (define-binary-type ucs-2-string (length)
   (:reader (in)
-           (let ((byte-order-mark (read-value 'u2 in))
-                 (characters (1- (/ length 2))))
-             (read-value
-              'generic-string in
-              :length characters
-              :character-type (ucs-2-char-type byte-order-mark))))
+    (let ((byte-order-mark (read-value 'u2 in))
+          (characters (1- (/ length 2))))
+      (read-value
+       'generic-string in
+       :length characters
+       :character-type (ucs-2-char-type byte-order-mark))))
   (:writer (out string)
-           (write-value 'u2 out #xfeff)
-           (write-value
-            'generic-string out string
-            :length (length string)
-            :character-type (ucs-2-char-type #xfeff))))
+    (write-value 'u2 out #xfeff)
+    (write-value
+     'generic-string out string
+     :length (length string)
+     :character-type (ucs-2-char-type #xfeff))))
 
 (define-binary-type ucs-2-terminated-string (terminator)
   (:reader (in)
-           (let ((byte-order-mark (read-value 'u2 in)))
-             (read-value
-              'generic-terminated-string in
-              :terminator terminator
-              :character-type (ucs-2-char-type byte-order-mark))))
+    (let ((byte-order-mark (read-value 'u2 in)))
+      (read-value
+       'generic-terminated-string in
+       :terminator terminator
+       :character-type (ucs-2-char-type byte-order-mark))))
   (:writer (out string)
-           (write-value 'u2 out #xfeff)
-           (write-value
-            'generic-terminated-string out string
-            :terminator terminator
-            :character-type (ucs2-char-type #xfeff))))
+    (write-value 'u2 out #xfeff)
+    (write-value
+     'generic-terminated-string out string
+     :terminator terminator
+     :character-type (ucs2-char-type #xfeff))))
 
 (defun read-id3 (file)
   (with-open-file (in file :element-type '(unsigned-byte 8))
@@ -149,32 +149,25 @@
   (with-open-file (in file :element-type '(unsigned-byte 8))
     (string= "ID3" (read-value 'iso-8859-1-string in :length 3))))
 
-(define-binary-class generic-frame (id3-frame)
-  ((data (raw-bytes :size size))))
-
 (define-binary-type raw-bytes (size)
   (:reader (in)
-           (let ((buf (make-array size :element-type '(unsigned-byte 8))))
-             (read-sequence buf in)
-             buf))
+    (let ((buf (make-array size :element-type '(unsigned-byte 8))))
+      (read-sequence buf in)
+      buf))
   (:writer (out buf)
-           (write-sequence buf out)))
-
-(defun find-frame-class (id)
-  (declare (ignore id))
-  'generic-frame)
+    (write-sequence buf out)))
 
 (define-condition in-padding () ())
 
 (define-binary-type frame-id (length)
   (:reader (in)
-           (let ((first-byte (read-byte in)))
-             (when (= first-byte 0) (signal 'in-padding))
-             (let ((rest (read-value 'iso-8859-1-string in :length (1- length))))
-               (concatenate
-                'string (string (code-char first-byte)) rest))))
+    (let ((first-byte (read-byte in)))
+      (when (= first-byte 0) (signal 'in-padding))
+      (let ((rest (read-value 'iso-8859-1-string in :length (1- length))))
+        (concatenate
+         'string (string (code-char first-byte)) rest))))
   (:writer (out id)
-           (write-value 'iso-8859-1-string out id :length length)))
+    (write-value 'iso-8859-1-string out id :length length)))
 
 (define-tagged-binary-class id3-frame ()
   ((id (frame-id :length 3))
@@ -197,16 +190,16 @@
 
 (define-binary-type optional (type if)
   (:reader (in)
-           (when if (read-value type in)))
+    (when if (read-value type in)))
   (:writer (out value)
-           (when if (write-value type out value))))
+    (when if (write-value type out value))))
 
 (define-binary-class id3v2.3-tag (id3-tag)
-                     ((extended-header-size (optional :type 'u4 :if (extended-p flags)))
-                      (extra-flags (optional :type 'u2 :if (extended-p flags)))
-                      (padding-size (optional :type 'u4 :if (extended-p flags)))
-                      (crc (optional :type 'u4 :if (crc-p flags extra-flags)))
-                      (frames (id3-frames :tag-size size :frame-type 'id3v2.3-frame))))
+  ((extended-header-size (optional :type 'u4 :if (extended-p flags)))
+   (extra-flags (optional :type 'u2 :if (extended-p flags)))
+   (padding-size (optional :type 'u4 :if (extended-p flags)))
+   (crc (optional :type 'u4 :if (crc-p flags extra-flags)))
+   (frames (id3-frames :tag-size size :frame-type 'id3v2.3-frame))))
 
 (defun extended-p (flags) (logbitp 6 flags))
 
@@ -215,22 +208,158 @@
 
 (define-binary-type id3-frames (tag-size frame-type)
   (:reader (in)
-           (loop with to-read = tag-size
-                 while (plusp to-read)
-                 for frame = (read-frame frame-type in)
-                 while frame
-                 do (decf to-read (+ (frame-header-size frame) (size frame)))
-                 collect frame
-                 finally (loop repeat (1- to-read) do (read-byte in))))
+    (loop with to-read = tag-size
+          while (plusp to-read)
+          for frame = (read-frame frame-type in)
+          while frame
+          do (decf to-read (+ (frame-header-size frame) (size frame)))
+          collect frame
+          finally (loop repeat (1- to-read) do (read-byte in))))
   (:writer (out frames)
-           (loop with to-write = tag-size
-                 for frame in frames
-                 do (write-value frame-type out frame)
-                    (decf to-write (+ (frame-header-size frame) (size frame)))
-                 finally (loop repeat to-write do (write-byte 0 out)))))
+    (loop with to-write = tag-size
+          for frame in frames
+          do (write-value frame-type out frame)
+             (decf to-write (+ (frame-header-size frame) (size frame)))
+          finally (loop repeat to-write do (write-byte 0 out)))))
 
 (defun read-frame (frame-type in)
   (handler-case (read-value frame-type in)
     (in-padding () nil)))
 
 (defgeneric frame-header-size (frame))
+
+(define-tagged-binary-class id3v2.2-frame ()
+  ((id (frame-id :length 3))
+   (size u3))
+  (:dispatch (find-frame-class id)))
+
+(define-tagged-binary-class id3v2.3-frame ()
+  ((id (frame-id :length 4))
+   (size u4)
+   (flags u2)
+   (decompressed-size (optional :type 'u4 :if (frame-compressed-p flags)))
+   (encryption-scheme (optional :type 'u1 :if (frame-encrypted-p flags)))
+   (grouping-identity (optional :type 'u1 :if (frame-grouped-p flags))))
+  (:dispatch (find-frame-class id)))
+
+(defun frame-compressed-p (flags) (logbitp 7 flags))
+
+(defun frame-encrypted-p (flags) (logbitp 6 flags))
+
+(defun frame-grouped-p (flags) (logbitp 5 flags))
+
+(defmethod frame-header-size ((frame id3v2.2-frame)) 6)
+
+(defmethod frame-header-size ((frame id3v2.3-frame)) 10)
+
+(define-binary-class generic-frame ()
+  ((data (raw-bytes :size (data-bytes (current-binary-object))))))
+
+(defgeneric data-bytes (frame))
+
+(defmethod data-bytes ((frame id3v2.2-frame))
+  (size frame))
+
+(defmethod data-bytes ((frame id3v2.3-frame))
+  (let ((flags (flags frame)))
+    (- (size frame)
+       (if (frame-compressed-p flags) 4 0)
+       (if (frame-encrypted-p flags) 1 0)
+       (if (frame-grouped-p flags) 1 0))))
+
+(define-binary-class generic-frame-v2.2 (id3v2.2-frame generic-frame) ())
+
+(define-binary-class generic-frame-v2.3 (id3v2.3-frame generic-frame) ())
+
+(defun frame-types (file)
+  (delete-duplicates (mapcar #'id (frames (read-id3 file))) :test #'string=))
+
+(defun frame-types-in-dir (dir)
+  (let ((ids ()))
+    (flet ((collect (file)
+             (setf ids (nunion ids (frame-types file) :test #'string=))))
+      (walk-directory dir #'collect :test #'mp3-p))
+    ids))
+
+(defun non-terminated-type (encoding)
+  (ecase encoding
+    (0 'iso-8859-1-string)
+    (1 'ucs-2-string)))
+
+(defun terminated-type (encoding)
+  (ecase encoding
+    (0 'iso-8859-1-terminated-string)
+    (1 'ucs-2-terminated-string)))
+
+(defun string-args (encoding length terminator)
+  (cond
+    (length
+     (values (non-terminated-type encoding) :length length))
+    (terminator
+     (values (terminated-type encoding) :terminator terminator))))
+
+(define-binary-type id3-encoded-string (encoding length terminator)
+  (:reader (in)
+    (multiple-value-bind (type keyword arg)
+        (string-args encoding length terminator)
+      (read-value type in keyword arg)))
+  (:writer (out string)
+    (multiple-value-bind (type keyword arg)
+        (string-args encoding length terminator)
+      (write-value type out string keyword arg))))
+
+(define-binary-class text-info-frame ()
+  ((encoding u1)
+   (information (id3-encoded-string :encoding encoding :length (bytes-left 1)))))
+
+(defun bytes-left (bytes-read)
+  (- (size (current-binary-object)) bytes-read))
+
+(define-binary-class text-info-frame-v2.2 (id3v2.2-frame text-info-frame) ())
+
+(define-binary-class text-info-frame-v2.3 (id3v2.3-frame text-info-frame) ())
+
+(defun find-frame-class (name)
+  (cond
+    ((and (char= (char name 0) #\T)
+          (not (member name '("TXX" "TXXX") :test #'string=)))
+     (ecase (length name)
+       (3 'text-info-frame-v2.2)
+       (4 'text-info-frame-v2.3)))
+    (t
+     (ecase (length name)
+       (3 'generic-frame-v2.2)
+       (4 'generic-frame-v2.3)))))
+
+(define-binary-class comment-frame ()
+  ((encoding u1)
+   (language (iso-8859-1-string :length 3))
+   (description (id3-encoded-string :encoding encoding :terminator +null+))
+   (text (id3-encoded-string
+          :encoding encoding
+          :length (bytes-left
+                   (+ 1 ; encoding
+                      3 ; language
+                      (encoded-string-length description encoding t)))))))
+
+(defun encoded-string-length (string encoding terminated)
+  (let ((characters (+ (length string) (if terminated 1 0))))
+    (* characters (ecase encoding (0 1) (1 2)))))
+
+(define-binary-class comment-frame-v2.2 (id3v2.2-frame comment-frame) ())
+
+(define-binary-class comment-frame-v2.3 (id3v2.3-frame comment-frame) ())
+
+(defun find-frame-class (name)
+  (cond
+    ((and (char= (char name 0) #\T)
+          (not (member name '("TXX" "TXXX") :test #'string=)))
+     (ecase (length name)
+       (3 'text-info-frame-v2.2)
+       (4 'text-info-frame-v2.3)))
+    ((string= name "COM") 'comment-frame-v2.2)
+    ((string= name "COMM") 'comment-frame-v2.3)
+    (t
+     (ecase (length name)
+       (3 'generic-frame-v2.2)
+       (4 'generic-frame-v2.3)))))
